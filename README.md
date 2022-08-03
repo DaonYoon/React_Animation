1db8e64936c776a6ce748f88d18b3f25b9572808
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2022-08-03 13:40:07 +0900

    Update
    Update

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Learn and dominate the React ecosystem once and for all.
 
-##### UPDATE 22-08-02 POWER README ######
-###########
+##### UPDATE 22-08-03 POWER README ######
+# <Box variants={Boxvar} whileHover={"hover"} whileTap={"click"}>
+
+      <BiggerBox ref={biggerBoxRef}>
+      <Box drag dragConstraints={biggerBoxRef} variants={Boxvar} whileHover={"hover"} whileDrag={"drag"} whileTap={"click"}>
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion"
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,63 +13,17 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255,0.2);
-  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-background-color: white;
-width: 70px;
-height: 70px;
-place-self: center;
-border-radius: 35px;
-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
-
-
-const Boxvar = {
-  start: {
-    opacity: 0,
-    scale: 0.5
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-}
-
-const CircleVar = {
-  start: {
-    opacity: 0,
-    y:10
-  },
-  end: {
-    opacity: 1,
-    y:0
-  }
-}
-//박스에 들어갈 variants 만드셈 start에 opacity 0 scale: 0.5, / end에 opactiy 1 , scale 1 transition: {type spring duration 0.5 bounce 0.5}
-
 function App() {
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   return (
     <Wrapper>
-      <Box variants={Boxvar} initial="start" animate="end">
-        <Circle variants={CircleVar}/>
-        <Circle variants={CircleVar}/>
-        <Circle variants={CircleVar}/>
-        <Circle variants={CircleVar}/>
-      </Box>
-      <motion.div></motion.div>
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
